4f196095528457c9a2d7750e87c1b4cd61c1f2e3
oxeek <57642600+oxeek@users.noreply.github.com>
2021-05-03 18:29:15 +0300

    *Add a new Example.txt to /source *button3 in to main form renamed to button343

--- Reporting v1.0/Form1.Designer.cs
+++ Reporting v1.0/Form1.Designer.cs
@@ -1242,7 +1242,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(56, 19);
             this.button3.TabIndex = 54;
-            this.button3.Text = "button3";
+            this.button3.Text = "button343";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Visible = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
